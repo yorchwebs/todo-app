6b63f17bbfe17867287d652cf2b03975909c6427
Jorge Garcia <yorchwebs@gmail.com>
2024-12-13 19:47:26 -0600

    feat(register): Update register view and template, and user_info template

--- users/views.py
+++ users/views.py
@@ -7,33 +7,6 @@ from django.contrib import messages
 
 
 # Create your views here.
-def login_view(request):
-    if request.method == "POST":
-        # Obtener los datos del formulario
-        username = request.POST["username"]
-        password = request.POST["password"]
-
-        # Autenticar al usuario
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            # Iniciar sesión si las credenciales son correctas
-            login(request, user)
-            return redirect("todo:index")  # Redirigir a la página principal
-        else:
-            # Mostrar un error si las credenciales no son válidas
-            messages.error(request, "Email o contraseña incorrectos.")
-
-    # Mostrar el formulario de inicio de sesión
-    return render(request, "users/login.html")
-
-
-@login_required
-def logout_view(request):
-        logout(request)
-        return redirect("accounts:login")
-
-
 def register_view(request):
     if request.method == "POST":
         # Obtener datos del formulario
@@ -65,16 +38,44 @@ def register_view(request):
             email=email,
             password=make_password(password),  # Hashear la contraseña
         )
+        user.save()
         messages.success(
-            request, "Usuario registrado exitosamente. ¡Ahora puedes iniciar sesión!"
+            request, "Usuario registrado exitosamente."
         )
         return redirect("todo:index")
     
     else:
-        messages.error(request, "Hubo un error al registrar el usuario.")
+        messages.error(request, "Las contraseñas no coinciden.")
         return render(request, "users/register.html")
 
     return render(request, "users/register.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        # Obtener los datos del formulario
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Autenticar al usuario
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # Iniciar sesión si las credenciales son correctas
+            login(request, user)
+            return redirect("todo:index")  # Redirigir a la página principal
+        else:
+            # Mostrar un error si las credenciales no son válidas
+            messages.error(request, "Email o contraseña incorrectos.")
+
+    # Mostrar el formulario de inicio de sesión
+    return render(request, "users/login.html")
+
+
+@login_required
+def logout_view(request):
+        logout(request)
+        return redirect("accounts:login")
 
 
 def user_view(request):
